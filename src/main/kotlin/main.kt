54260b7b5f673df4f4cca61c37d8b4175f212836
yaninalex2003 <yanin.alex2003@yandex.ru>
2021-10-19 18:51:17 +0300

    1)Now image downloaded in file

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,9 +5,13 @@ import org.jetbrains.skija.*
 import org.jetbrains.skiko.SkiaLayer
 import org.jetbrains.skiko.SkiaRenderer
 import org.jetbrains.skiko.SkiaWindow
+import org.jetbrains.skiko.toBufferedImage
 import java.awt.Dimension
 import java.awt.event.MouseEvent
 import java.awt.event.MouseMotionAdapter
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
 import javax.swing.WindowConstants
 import kotlin.math.cos
 import kotlin.math.min
@@ -15,23 +19,30 @@ import kotlin.math.sin
 
 
 fun main() {
+    println("Введите тип диаграммы")
     val diagram = readLine()!!
-    if (diagram != "circle" && diagram != "step" && diagram != "petals") throw WrongTypeOfDiagram()
+    println("Введите название файла для сохранения диаграммы")
+    val fileName = readLine()!!
+    val fileNameNew = fileNew(fileName)
+    println("Введите название объектов и числовые значения")
+    val s = fileName.length
+    if (s < 3)
+        if (diagram != "circle" && diagram != "step" && diagram != "petals") throw WrongTypeOfDiagram()
     val m: MutableMap<String, Float> = mutableMapOf()
     while (true) {
         val value = readLine() ?: break
         val number: String = readLine() ?: throw NoNumericalValue()
         m += value to number.toFloat()
     }
-    createWindow("pf-2021-viz", m, diagram)
+    createWindow("pf-2021-viz", m, diagram, fileNameNew)
 }
 
-fun createWindow(title: String, m: Map<String, Float>, s: String) = runBlocking(Dispatchers.Swing) {
+fun createWindow(title: String, m: Map<String, Float>, s: String, fileName: String) = runBlocking(Dispatchers.Swing) {
     val window = SkiaWindow()
     window.defaultCloseOperation = WindowConstants.DISPOSE_ON_CLOSE
     window.title = title
 
-    window.layer.renderer = Renderer(window.layer, m, s)
+    window.layer.renderer = Renderer(window.layer, m, s, fileName)
     window.layer.addMouseMotionListener(MyMouseMotionAdapter)
 
     window.preferredSize = Dimension(800, 600)
@@ -41,7 +52,7 @@ fun createWindow(title: String, m: Map<String, Float>, s: String) = runBlocking(
     window.isVisible = true
 }
 
-class Renderer(val layer: SkiaLayer, m: Map<String, Float>, s: String) : SkiaRenderer {
+class Renderer(val layer: SkiaLayer, m: Map<String, Float>, s: String, filename: String) : SkiaRenderer {
     val typeface = Typeface.makeFromFile("fonts/JetBrainsMono-Regular.ttf")
     val font = Font(typeface, 20f)
     val black = Paint().apply {
@@ -84,16 +95,23 @@ class Renderer(val layer: SkiaLayer, m: Map<String, Float>, s: String) : SkiaRen
         mode = PaintMode.FILL
         strokeWidth = 3f
     }
+    val white = Paint().apply {
+        color = 0xffffffff.toInt()
+        mode = PaintMode.FILL
+        strokeWidth = 3f
+    }
     val ourMap = m
     val vid = s
     val paints: List<Paint> = listOf(black, purple, blue, green, grey, red, yellow, greyLite)
     val names: List<String> = listOf("black", "purple", "blue", "green", "grey", "red", "yellow", "lite grey")
+    val outputFile = File(filename)
     override fun onRender(canvas: Canvas, width: Int, height: Int, nanoTime: Long) {
         val contentScale = layer.contentScale
         canvas.scale(contentScale, contentScale)
         val w = (width / contentScale).toInt()
         val h = (height / contentScale).toInt()
         // РИСОВАНИЕ
+        canvas.drawRect(Rect(0F, 0F, w.toFloat(), h.toFloat()), white)
         if (vid == "circle") {
             val left = w - 410f
             val top = h - 410f
@@ -101,11 +119,11 @@ class Renderer(val layer: SkiaLayer, m: Map<String, Float>, s: String) : SkiaRen
             val bottom = h.toFloat() - 10f
             val angles = anglesForCircleDiagram(ourMap)
             for (i in 0 until ourMap.size) {
-                canvas.drawArc(left, top, right, bottom, angles[i], angles[i + 1] - angles[i], true, paints[i])
+                canvas.drawArc(left, top, right, bottom, angles[i], angles[i + 1] - angles[i], true, paints[i%8])
             }
             var h1 = 30F
             for ((ind, i) in ourMap.keys.withIndex()) {
-                canvas.drawString(names[ind] + " - $i", 20F, h1, font, paints[ind])
+                canvas.drawString(names[ind] + " - $i", 20F, h1, font, paints[ind%8])
                 h1 += 30F
             }
         }
@@ -127,7 +145,7 @@ class Renderer(val layer: SkiaLayer, m: Map<String, Float>, s: String) : SkiaRen
             val points = coordinates(ourMap, startx, starty)
             var h1 = 30F
             for ((ind, i) in points.withIndex()) {
-                canvas.drawLine(startx, starty, i.first, i.second, paints[ind])
+                canvas.drawLine(startx, starty, i.first, i.second, paints[ind%8])
             }
             for (i in points.indices) {
                 if (i == points.size - 1) {
@@ -142,11 +160,14 @@ class Renderer(val layer: SkiaLayer, m: Map<String, Float>, s: String) : SkiaRen
             }
             canvas.drawCircle(startx, starty, 3f, greyLite)
             for ((ind, i) in ourMap.keys.withIndex()) {
-                canvas.drawString(names[ind] + " - $i", 20F, h1, font, paints[ind])
+                canvas.drawString(names[ind] + " - $i", 20F, h1, font, paints[ind%8])
                 h1 += 30F
             }
         }
         layer.needRedraw()
+        val bitmap = layer.screenshot() ?: return
+        val image: BufferedImage = bitmap.toBufferedImage()
+        ImageIO.write(image, "png", outputFile)
     }
 }
 
@@ -164,7 +185,7 @@ object MyMouseMotionAdapter : MouseMotionAdapter() {
 }
 
 fun minValueInMap(m: Map<String, Float>): Float {
-    var minInMap=0F
+    var minInMap = 0F
     var x = true
     for (i in m.values) {
         if (x) {
@@ -196,4 +217,13 @@ fun coordinates(m: Map<String, Float>, startx: Float, starty: Float): List<Pair<
         angle += 2 * Math.PI.toFloat() / m.size
     }
     return ans
+}
+
+fun fileNew(fileName: String): String {
+    val s = fileName.length
+    val fileNameNew = "$fileName.png"
+    return if (s < 4) fileNameNew
+    else if (fileName[s - 4] == '.' && fileName[s - 3] == 'p' && fileName[s - 2] == 'n' && fileName[s - 1] == 'g') fileName
+    else fileNameNew
+
 }
